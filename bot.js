d1f1bc39e4a21dcf1671bf02c09c6f82649c5b77
KUkWvpVdu5RRq1aGhj6YvZK0RDJoYOOGvnzqRbc <46111129+KUkWvpVdu5RRq1aGhj6YvZK0RDJoYOOGvnzqRbc@users.noreply.github.com>
2018-12-24 18:16:37 +0000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+});
+
+client.on('ready', async() => {
+var server = "520248820374700034"; //
+var channel = "520494880330481665";//
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein**')
+    },305);
+ });
+
+ 
+client.login("NTI2ODE5NDI0NTczOTgwNjc0.DwKu9Q.JrUcol5k17McfkcZerqvBXD0tYY");
